5fc67d8e253240021f237553e44add8aca7837d3
lnkz80 <lnkz80@gmail.com>
2021-03-12 15:15:40 +0200

    start&sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import logo from './assets/img/logo.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="app">
+     <div className="app__sidebar">
+       {/* <img src={logo} alt="logo" /> */}
+        <h3>SideBar</h3>        
+     </div>
+     <div className="app__content">
+      <h3>Content</h3>
+     </div>
+   </div>
   );
 }
 
